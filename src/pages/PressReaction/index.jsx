59959a3e4a8ec80722b09b9826bd5cf8225e23e1
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-09-05 11:12:37 -0300

    Changed the clickable color and implemented a simple way of getting the reaction time, but it seems slower than other apps, so I'll research about making a react application faster

--- src/pages/PressReaction/index.jsx
+++ src/pages/PressReaction/index.jsx
@@ -15,10 +15,15 @@ const rando = randojs.rando;
 
 function PressReaction() {
   const [started, setStarted] = useState(false);
+
   const [counter, setCounter] = useState(3);
   const [countdown, setCountdown] = useState(null);
+
   const [clickable, setClickable] = useState(false);
   const [clickTimer, setClickTimer] = useState(null);
+
+  const [timeWhenClickable, setTimeWhenClickable] = useState(null);
+
 
   useEffect(() => {
     document.title = "Press Reaction";
@@ -40,19 +45,17 @@ function PressReaction() {
   const quit = useCallback(() => {
     console.log("quit");
     setStarted(false);
+
     clearTimeout(clickTimer);
-    console.log("before setClickable false");
     setClickable(false);
-    console.log("after setClickable false");
+
     clearTimeout(countdown);
     setCounter(3);
   }, [countdown, clickTimer]);
 
   useEffect(() => {
     if(started) {
-      console.log("before setClickable true");
       setClickTimer(setTimeout(() => setClickable(true), rando(5000, 14000)));
-      console.log("after setClickable true");
     }
   }, [started]);
 
@@ -62,12 +65,20 @@ function PressReaction() {
   }, [started, counter]);
 
   useEffect(() => {
-    console.log(clickable);
-  }, [clickable]);
+    if(started && clickable)
+      setTimeWhenClickable(Date.now());
+  }, [started, clickable]);
 
-  const clickedCircle = useCallback(() => {
+  const circleClicked = useCallback(() => {
+    if(!clickable) {
+      console.log("It wasn't clickable, one second penalty");
+    } else {
+      const timeWhenClicked = Date.now();
+      const reactionTime = timeWhenClicked - timeWhenClickable;
+      console.log(reactionTime);
+    }
     console.log(`circle was clicked when clickable is ${clickable}`);
-  }, [clickable]);
+  }, [clickable, timeWhenClickable]);
 
   return (
     <Container>
@@ -79,7 +90,7 @@ function PressReaction() {
 
         {counter === 0 && <CircleToBeClickedASAP 
           clickable={clickable} 
-          onClick={clickedCircle} 
+          onClick={circleClicked} 
         />}
 
         {/* <CircleToBeClickedASAP clickable={clickable} /> */}
